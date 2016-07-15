d00e3c82790966a38bf0be963c00feebad54f8b0
Sultanxda <sultanxda@gmail.com>
2016-07-15 16:24:25 -0700

    input: synaptics: s3320: Don't run baseline on resume
    This causes the first touch after resume to get ignored, so don't do it.
    
    Change-Id: Ia6192181f9be053f75b04046ba0c97d6dca26c67
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -4134,7 +4134,10 @@ static int fb_notifier_callback(struct notifier_block *self, unsigned long event
             {
                 TPD_DEBUG("%s going TP resume start\n", __func__);
                 ts->is_suspended = 0;
-				queue_delayed_work(get_base_report, &ts->base_work,msecs_to_jiffies(1));
+		if (ts->gesture_enable)
+			synaptics_enable_interrupt_for_gesture(ts, false);
+		atomic_set(&ts->is_stop, 0);
+		touch_enable(ts);
 				synaptics_ts_resume(&ts->client->dev);
                 //atomic_set(&ts->is_stop,0);
                 TPD_DEBUG("%s going TP resume end\n", __func__);
