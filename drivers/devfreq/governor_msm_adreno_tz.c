7683111a100590083c415e90b2308532c9e3f9d3
tbalden <illespal@gmail.com>
2016-07-30 12:48:59 -0700

    adrenoboost: finetuning algorithm - scale it a bit down
    Values range from 0-3, 0 off, 3 most aggressive.
    Scales back nicely in 2d to 133mhz.
    Only scale up and count if MIN_VALUE crossed
    
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -56,7 +56,10 @@ static DEFINE_SPINLOCK(suspend_lock);
 
 #define TAG "msm_adreno_tz: "
 
-static unsigned int adrenoboost = 10000;
+#if 1
+static unsigned int adrenoboost = 1;
+#endif
+
 static u64 suspend_time;
 static u64 suspend_start;
 static unsigned long acc_total, acc_relative_busy;
@@ -87,6 +90,7 @@ u64 suspend_time_ms(void)
 	return time_diff;
 }
 
+#if 1
 static ssize_t adrenoboost_show(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
@@ -101,7 +105,7 @@ static ssize_t adrenoboost_save(struct device *dev,
 {
 	int input;
 	sscanf(buf, "%d ", &input);
-	if (input < 0 || input > 50000) {
+	if (input < 0 || input > 3) {
 		adrenoboost = 0;
 	} else {
 		adrenoboost = input;
@@ -109,7 +113,7 @@ static ssize_t adrenoboost_save(struct device *dev,
 
 	return count;
 }
-
+#endif
 
 static ssize_t gpu_load_show(struct device *dev,
 		struct device_attribute *attr,
@@ -156,8 +160,10 @@ static ssize_t suspend_time_show(struct device *dev,
 	return snprintf(buf, PAGE_SIZE, "%llu\n", time_diff);
 }
 
+#if 1
 static DEVICE_ATTR(adrenoboost, 0644,
 		adrenoboost_show, adrenoboost_save);
+#endif
 
 static DEVICE_ATTR(gpu_load, 0444, gpu_load_show, NULL);
 
@@ -168,7 +174,9 @@ static DEVICE_ATTR(suspend_time, 0444,
 static const struct device_attribute *adreno_tz_attr_list[] = {
 		&dev_attr_gpu_load,
 		&dev_attr_suspend_time,
+#if 1
 		&dev_attr_adrenoboost,
+#endif
 		NULL
 };
 
@@ -314,7 +322,16 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 
 	*freq = stats.current_frequency;
 	priv->bin.total_time += stats.total_time;
+#if 1
+	// scale busy time up based on adrenoboost parameter, only if MIN_BUSY exceeded...
+	if ((unsigned int)(priv->bin.busy_time + stats.busy_time) >= MIN_BUSY) {
+		priv->bin.busy_time += stats.busy_time * (1 + (adrenoboost*3)/2);
+	} else {
+		priv->bin.busy_time += stats.busy_time;
+	}
+#else
 	priv->bin.busy_time += stats.busy_time;
+#endif
 
 	/* Update the GPU load statistics */
 	compute_work_load(&stats, priv, devfreq);
@@ -346,7 +363,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq,
 
 		scm_data[0] = level;
 		scm_data[1] = priv->bin.total_time;
-		scm_data[2] = priv->bin.busy_time + (level * adrenoboost);
+		scm_data[2] = priv->bin.busy_time;
 		__secure_tz_update_entry3(scm_data, sizeof(scm_data),
 					&val, sizeof(val), priv->is_64);
 	}
