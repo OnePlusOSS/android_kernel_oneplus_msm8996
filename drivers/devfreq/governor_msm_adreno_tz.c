216ef48b5684062c6fc8312eefe6474a8d8d6f01
flar2 <asegaert@gmail.com>
2016-09-11 16:09:11 -0400

    adrenoboost: disable by default

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -57,7 +57,7 @@ static DEFINE_SPINLOCK(suspend_lock);
 #define TAG "msm_adreno_tz: "
 
 #if 1
-static unsigned int adrenoboost = 1;
+static unsigned int adrenoboost = 0;
 #endif
 
 static u64 suspend_time;
