750c220343f5a54e1a703bc663de00803038747c
engstk <eng.stk@sapo.pt>
2016-12-08 03:57:42 +0000

    input: synaptics: s3320: fix gestures
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -1180,7 +1180,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 				(gesture_buffer[2] == 0x6d) ? Mgestrue :
 				UnkownGestrue;
 	}
-
+/*
 	keyCode = UnkownGestrue;
 	// Get key code based on registered gesture.
 	switch (gesture) {
@@ -1208,7 +1208,7 @@ static void gesture_judge(struct synaptics_ts_data *ts)
 		default:
 			break;
 	}
-
+*/
 	TPD_ERR("detect %s gesture\n", gesture == DouTap ? "(double tap)" :
 			gesture == UpVee ? "(V)" :
 			gesture == DownVee ? "(^)" :
