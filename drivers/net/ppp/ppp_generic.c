6eade7501814feef2a8e221c053a8f8c8b7e1099
NewEraCracker <neweracracker@gmail.com>
2016-12-26 23:08:24 +0000

    Revert "UPSTREAM: ppp: defer netns reference release for ppp channel"
    This reverts commit f1aba600087274ebeb2f6867b55292338acdc8b2.
    
    (cherry picked from commit 851270d777073442de83ba13c99036b2027ec242)

--- drivers/net/ppp/ppp_generic.c
+++ drivers/net/ppp/ppp_generic.c
@@ -2957,9 +2957,6 @@ ppp_disconnect_channel(struct channel *pch)
  */
 static void ppp_destroy_channel(struct channel *pch)
 {
-	put_net(pch->chan_net);
-	pch->chan_net = NULL;
-
 	atomic_dec(&channel_count);
 
 	if (!pch->file.dead) {
